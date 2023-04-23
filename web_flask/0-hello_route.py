72dd51f32d305cfcd531b7e0b00b9f83e5595fa8
ThengweTheFirst <sithebemzwandie40@gmail.com>
2023-04-23 21:22:02 +0200

    advance

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""
+Starts a Flask web application.
 The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'
 """
 from flask import Flask
 
@@ -11,8 +10,10 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return ("Hello HBNB!")
+    """
+    Displays 'Hello HBNB!'
+    """
+    return ('Hello HBNB!')
 
 
 if __name__ == "__main__":
